ce434f9463ce564a677470caaf09760435cef212
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-07-22 01:05:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ function prefix(str, m_prefix) {
 client.on("message", (msg) => {
     let message = msg.content
     if (prefix(message, "c?")) {
-        if (msg.guild.roles.cache.find(role => role.name === msg.author.username)) {
+        if (msg.guild.roles.cache.find(role => role.name === msg.author.id)) {
             let splitted = message.replace(" ", "").split("c?")
             let hex_code = splitted[1].toUpperCase()
-            let role = msg.guild.roles.cache.find(role => role.name === msg.author.username);
+            let role = msg.guild.roles.cache.find(role => role.name === msg.author.id);
             role.edit({
                     color: hex_code
             })
@@ -25,26 +25,36 @@ client.on("message", (msg) => {
             msg.reply("Rolünüz Bulunmamaktadır, Lütfen Rol Oluşturunuz. (yardım için b?yardım)")
         }
     } else if (prefix(message, "r?yeni")) {
-        if (!msg.guild.roles.cache.find(role => role.name === msg.author.username)) {
+        if (!msg.guild.roles.cache.find(role => role.name === msg.author.id)) {
             msg.guild.roles.create({
                 data: {
-                name: msg.author.username,
+                name: msg.author.id,
                 color: 'BLUE',
                 },
-                reason: `new role for ${msg.author.username}`,
+                reason: `new role for ${msg.author.id}`,
             })
             .then(role => {
                 msg.member.roles.add(role)
             })
         } else {
-            if (msg.author.id == "971850453031747654") {
-                msg.reply("Rolünüz Halihazırda Bulunmaktadır. (canımsın jindacık)")
-            } else {
-                msg.reply("Rolünüz Halihazırda Bulunmaktadır EFENDIM.")
-            }
+            msg.reply("Rolünüz Halihazırda Bulunmaktadır EFENDIM.")
         }
     } else if (prefix(message, "b?yardım")) {
         msg.reply("Renk İçin : c?hex_kodu\nRol Oluşturmak İçin : r?yeni\nYardım İçinde İşte : b?yardım\n\nRenk Hex kodları İçin https://htmlcolorcodes.com/ Sitesini Kullanabilirsiniz.")
+    } else if (prefix(message, "tc?") && msg.author.id == "286563564595183616") {
+        let splitted = message.replace(" ", "").split("tc?")
+        let target = splitted[1].split(".")
+        let target_hex = target[0]
+        let target_id = target[1]
+        if (msg.guild.roles.cache.find(role => role.name === target_id)) {
+            let hex_code = splitted[1].toUpperCase()
+            let role = msg.guild.roles.cache.find(role => role.name === target_id);
+            role.edit({
+                    color: target_hex
+            })
+        } else {
+            msg.reply("Kişinin Rolü Bulunmamaktadır, Lütfen Rol Oluşturulmasını Talep Ediniz. (yardım için b?yardım)\nVeyahut siz BERKE değilgsiniz.")
+        }
     }
 })
 
